bc9f1bb3e6b1aa2f19e48e8b43301fe421cd5e28
Tijani <ayomidetijani6@gmail.com>
2020-09-03 11:24:38 +0100

    'Update'

--- user/forms.py
+++ user/forms.py
@@ -5,5 +5,6 @@ from django.contrib.auth.forms import UserCreationForm
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
 
-    model = User
-    field = ['username', 'email', 'password1', 'password2']
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
